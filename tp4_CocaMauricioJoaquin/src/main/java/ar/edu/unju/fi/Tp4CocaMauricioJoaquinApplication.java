d84906b5c65a139a0c193896f374248c0a9d9013
seba_ <seba_@Compu>
2024-05-22 16:01:24 -0300

    inicializacion del proyecto

---
+++ tp4_CocaMauricioJoaquin/src/main/java/ar/edu/unju/fi/Tp4CocaMauricioJoaquinApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4CocaMauricioJoaquinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4CocaMauricioJoaquinApplication.class, args);
+	}
+
+}
